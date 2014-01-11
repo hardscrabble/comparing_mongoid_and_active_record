ea841719b104d44b40f5cc40077b70d7b08d08a9
Max Jacobson <max@hardscrabble.net>
2014-01-11 17:57:23 -0500

    init second app with mongoid

---
+++ todo_list_with_mongoid/config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+TodoList::Application.config.secret_key_base = 'de3eaef88708822b62e4c5a8d3a46dd9e224fa45199249d98f286e81ecb20bf150ce0eaf5021d66f41bf02ee986abad639caea92afa87da696ba7678c9633d9b'
